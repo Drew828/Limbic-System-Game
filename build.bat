da81471a9f5ab8231db09c69f872c63d4edd55f2
Drew828 <Drew828@users.noreply.github.com>
2026-05-24 21:27:05 -0400

    better run

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 @echo off
+echo Cleaning old build...
+if exist "dist\LimbicJourney" (
+    attrib -r -s -h "dist\LimbicJourney\*.*" /s /d >nul 2>&1
+    rmdir /s /q "dist\LimbicJourney"
+)
+if exist "build\LimbicJourney" rmdir /s /q "build\LimbicJourney"
+
 echo Building LimbicJourney...
 "C:/Users/drewm/AppData/Local/Programs/Python/Python313/python.exe" -m PyInstaller LimbicJourney.spec --noconfirm
 echo.
